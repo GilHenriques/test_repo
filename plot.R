1c3c80359780a2c06aa2dbccf029d8e19edecfcd
Gil Henriques <gilhenris@gmail.com>
2021-03-09 11:40:35 -0800

    Add a title and reverse y axis

--- plot.R
+++ plot.R
@@ -8,4 +8,6 @@ dat <- read.csv("https://raw.githubusercontent.com/EDUCE-UBC/educer/main/data-ra
 # create plot of oxygen by depth
 quickplot(data = dat,
           x = O2_uM,
-          y = Depth_m)
+          y = Depth_m,
+          main = "Depth profile of Oxygen concentration") +
+  scale_y_reverse()
