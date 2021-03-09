df6b5e0f17c6669f90b1f8857382a92ed4a3a1d6
Gil Henriques <gilhenris@gmail.com>
2021-03-09 11:33:27 -0800

    Create new project and plot oxygen vs depth

---
+++ plot.R
@@ -0,0 +1,11 @@
+# load packages
+## Suite of packages for data manipulation and visualization
+library(tidyverse)
+
+# load data from our GitHub
+dat <- read.csv("https://raw.githubusercontent.com/EDUCE-UBC/educer/main/data-raw/data_intro_ws.csv")
+
+# create plot of oxygen by depth
+quickplot(data = dat,
+          x = O2_uM,
+          y = Depth_m)
